28d1aa4c22501664dcc7face88a84485b679b2ee
Mitchell Bell <s3589418@student.rmit.edu.au>
2018-04-08 15:14:35 +1000

    Added some rough swipe controls.
    Will need to tweak with the public settings to find the right smoothness.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
 
+    public float scrollSpeed;
+    public float swipeDrag;
+    public float swipeNullPoint;
+
+    private Vector3 tapPosition;
+    private Vector3 startTapPosition;
+    private Vector3 newMousePosition;
+    private Vector3 swipeVelocity;
+    Vector3 swipeDir;
+
     // Use this for initialization
     void Start()
     {
@@ -14,6 +24,30 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            tapPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+            startTapPosition = tapPosition;
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            newMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+            swipeDir = newMousePosition - tapPosition;
+            tapPosition = newMousePosition;
+            Camera.main.transform.Rotate(new Vector3(swipeDir.y * scrollSpeed, -swipeDir.x * scrollSpeed, 0));
+        }
+
+        if(!Input.GetMouseButton(0))
+        {
+            Debug.Log(swipeDir.magnitude);
+            if(swipeDir.magnitude*100 > swipeNullPoint)
+            {
+                Camera.main.transform.Rotate(new Vector3(swipeDir.y * scrollSpeed, -swipeDir.x * scrollSpeed, 0));
+                swipeDir.Scale(new Vector3(swipeDrag, swipeDrag, 0));
+            }
+        }
 
     }
 }
