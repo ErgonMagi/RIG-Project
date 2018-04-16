6822e83080b96361384fd03f004be647acc361a0
Mitchell Bell <s3589418@student.rmit.edu.au>
2018-04-16 16:11:12 +1000

    Changed the computer setup a bit

--- Assets/Scripts/ComputerScreen.cs
+++ Assets/Scripts/ComputerScreen.cs
@@ -4,24 +4,15 @@ using UnityEngine;
 
 public class ComputerScreen : MonoBehaviour, ClickableObject {
 
-    CameraManager cam;
-    public Camera computerCam;
+    GameController gameController;
 
 	// Use this for initialization
 	void Start () {
-        cam = FindObjectOfType<CameraManager>();
+        gameController = FindObjectOfType<GameController>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-        
-
-	}
-
     public void onClick()
     {
-        cam.lerpToLoc(new Vector3(0, 1, -5.2f), new Vector3(0, 0, 0), 1.0f);
-        //cam.swapCamAfterLerp(computerCam);
+        gameController.toComputer();
     }
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public Camera computerCam;
+
+    private enum Gamestate
+    {
+        desk, computer
+    }
+
+    private Gamestate gamestate;
+    private CameraManager cam;
+    
+
+    // Use this for initialization
+    void Start () {
+        gamestate = Gamestate.computer;
+        cam = FindObjectOfType<CameraManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+
+    public void toComputer()
+    {
+        cam.lerpToLoc(new Vector3(0, 1, -5.2f), new Vector3(0, 0, 0), 1.0f);
+        cam.swapCamAfterLerp(computerCam);
+    }
+
+    public void fromComputer()
+    {
+        cam.lerpToLoc(new Vector3(0, 1, -10f), new Vector3(0, 0, 0), 1.0f);
+    }
+}
