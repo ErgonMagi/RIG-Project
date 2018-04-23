a0602ed282ff65c221fc2b2770459b9973c6214a
Mitchell Bell <s3589418@student.rmit.edu.au>
2018-04-23 14:22:55 +1000

    Can now set actors to a movie, no calculations yet

--- Assets/Scripts/ActorPicture.cs
+++ Assets/Scripts/ActorPicture.cs
@@ -8,6 +8,7 @@ public class ActorPicture : MonoBehaviour, ClickableObject {
     private bool selected;
     private Vector3 startPos;
     private CameraManager cameraManager;
+    private GameObject lockedPosition;
 
 	// Use this for initialization
 	void Start () {
@@ -22,6 +23,10 @@ public class ActorPicture : MonoBehaviour, ClickableObject {
         {
             this.transform.position = cameraManager.getCam().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraManager.getCam().nearClipPlane));
         }
+        else if(lockedPosition != null)
+        {
+            this.transform.position = lockedPosition.transform.position;
+        }
 	}
 
     public void setActor(Actor a)
@@ -33,12 +38,36 @@ public class ActorPicture : MonoBehaviour, ClickableObject {
     public void onClick()
     {
         selected = true;
-        Debug.Log("clicked");
     }
 
     private void OnMouseUp()
     {
         selected = false;
+
+        Vector3 mousePos = cameraManager.getCam().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraManager.getCam().nearClipPlane));
+
+        Vector3 dirFromCam = new Vector3(0, 0, 1);
+        if (!cameraManager.getCam().orthographic)
+        {
+            dirFromCam = mousePos - cameraManager.getCam().transform.position;
+        }
+
+        RaycastHit[] hit = Physics.RaycastAll(mousePos, dirFromCam, 15.0f, 1 << LayerMask.NameToLayer("ActorSlot"));
+
+        
+
+        foreach(RaycastHit h in hit)
+        {
+            if (h.collider.gameObject.layer == LayerMask.NameToLayer("ActorSlot"))
+            {
+                lockedPosition = h.collider.gameObject;
+            }
+        }
+        if(hit.Length == 0)
+        {
+            lockedPosition = null;
+        }
+
         this.transform.position = startPos;
     }
 }
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -132,6 +132,7 @@ public class InputManager : MonoBehaviour
                 }
             }
             objectSelected = false;
+            clickedObject = null;
         }
 
     }
