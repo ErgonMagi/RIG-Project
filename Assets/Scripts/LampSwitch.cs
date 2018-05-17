24efcab8e8a8d30f8f2d0d999b08cb53be5d73a7
Mitchell Bell <s3589418@student.rmit.edu.au>
2018-05-17 16:22:45 +1000

    Light is now switchable

---
+++ Assets/Scripts/LampSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampSwitch : MonoBehaviour, ClickableObject{
+
+    public Light lampLight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onClick()
+    {
+        lampLight.enabled = !lampLight.enabled;
+    }
+}
